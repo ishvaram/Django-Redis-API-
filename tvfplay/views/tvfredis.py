7ce4bb3d55ef9c494fd904ccec800cc679e8af1d
ishvaram <jehovaram@gmail.com>
2016-11-05 21:00:02 +0530

    added code generalization

--- tvfplay/views/tvfredis.py
+++ tvfplay/views/tvfredis.py
@@ -1,96 +1,126 @@
-import os, sys, string, json, ast
-import re
-import math
-import datetime
-import urllib
-import collections
-from django.db import connections
-from django.http import HttpResponse,JsonResponse
-from django.core.serializers.json import json, DjangoJSONEncoder
-from common_views import *
+'''
+!/usr/bin/python
+-*- coding: utf-8 -*-
+Author: Kovalan R
+Date: 05-11-2016
+Description: Redis-Django API - its connects to
+redis and does the given operations
+'''
+import sys
+import json
+import ast
 import redis
-import unicodedata
+from django.http import HttpResponse
+from django.core.serializers.json import json
+from django.core.serializers.json import DjangoJSONEncoder
+
+try:
+	REDIS_CONN = redis.StrictRedis(host='localhost', port=6379, db=1)
+except Exception, exc:
+	print "Unable to connect to Redis.Please check the redis service is running"
+	sys.exit(0)
 
 
-redis_con = redis.StrictRedis(host='localhost', port=6379, db=1)
+def index():
+	''' Index request Funtion
+	'''
+	return HttpResponse("Welcome to  TVF API Redis-Django")
 
-def index(request):
-  	return ("Welcome to Redis TVF API")
 
-
-def all_items(request):
+def all_items():
 	'''	List all the the Dict from Redis
 	'''
 	try:
-		if redis_con:
-			datum = redis_con.get('tvf_json')
+		if REDIS_CONN:
+			datum = REDIS_CONN.get('tvf_json')
 			datum = ast.literal_eval(datum)
-			return HttpResponse(json.dumps(datum, cls=DjangoJSONEncoder), content_type='application/json', status=200)
+			return HttpResponse(json.dumps(datum, cls=DjangoJSONEncoder), \
+				content_type='application/json', status=200)
 		else:
-			return HttpResponse("Not able to connect To Redis", status=200)
+			return HttpResponse("Not able to connect To Redis", status=500)
 
-	except Exception, e:
-		response = {'status':'Request Failure','message':e}
-		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=500)
+	except Exception, exc:
+		response = {'status': 'Request Failure', 'message': exc}
+		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), \
+			content_type='application/json', status=500)
 
 
-def category(request):
+def category():
 	'''	List all the Categories
 	'''
 	categories = {'categories': []}
 	try:
-		if redis_con:
-			datum = redis_con.get('tvf_json')
+		if REDIS_CONN:
+			datum = REDIS_CONN.get('tvf_json')
 			datum = ast.literal_eval(datum)
-			for x in datum:
-				categories['categories'].append(x['category_name'])
-			return HttpResponse(json.dumps(categories, cls=DjangoJSONEncoder), content_type='application/json', status=200)
+			for each in datum:
+				categories['categories'].append(each['category_name'])
+			return HttpResponse(json.dumps(categories, cls=DjangoJSONEncoder),\
+			 content_type='application/json', status=200)
 		else:
-			return HttpResponse("Not able to connect To Redis", status=200)
+			return HttpResponse("Not able to connect To Redis", status=500)
 
-	except Exception, e:
-		response = {'status':'Request Failure','message':e}
-		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=500)
+	except Exception, exc:
+		response = {'status': 'Request Failure', 'message': exc}
+		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder),\
+		 		content_type='application/json', status=500)
+
 
 def listbytype(request):
-	''' List all the items by type
+	''' List all the items by type either video or story
 	'''
-	sortbytype = {'result':[]}
+	sortbytype = {'result': []}
 	try:
-		datum = redis_con.get('tvf_json')
+		datum = REDIS_CONN.get('tvf_json')
 		datum = ast.literal_eval(datum)
 		if 'type' not in request.GET:
-			response = {'status':'Request Failure','message':'Incorrect request. The Type param is missing/invalid in the request'}
-			return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=403)
+			response = {'status': 'Request Failure', \
+				'message': 'Incorrect request. The Type param \
+				is missing/invalid in the request'}
+			return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), \
+					content_type='application/json', status=400)
 		else:
 			if request.GET['type'] == '':
-				response = {'status':'Request Failure','message':'Incorrect request. The Type param is missing/invalid in the request'}
-				return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=403)
-		for x in datum:
+				response = {'status': 'Request Failure', \
+					'message': 'Incorrect request. The Type param is \
+					missing/invalid in the request'}
+				return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder),\
+				 		content_type='application/json', status=400)
+		for each in datum:
 			if request.GET['type'] == 'story':
-				if x['type'] == 'story':
-					sortbytype['result'].append(x)
+				if each['type'] == 'story':
+					sortbytype['result'].append(each)
 			else:
-				if x['type'] == 'video':
-					sortbytype['result'].append(x)
-		return HttpResponse(json.dumps(sortbytype, cls=DjangoJSONEncoder), content_type='application/json', status=200)
-	except Exception, e:
-		response = {'status':'Request Failure','message':e}
-		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=500)
+				if each['type'] == 'video':
+					sortbytype['result'].append(each)
+		return HttpResponse(json.dumps(sortbytype, cls=DjangoJSONEncoder), \
+		 		content_type='application/json', status=200)
+	except Exception, exc:
+		response = {'status': 'Request Failure', 'message': exc}
+		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), \
+		 		content_type='application/json', status=500)
 
 
 def ratingsort(request):
-	sortbyratings = {'result':[]}
+	''' Sort the data By ascending and desc
+	'''
+	sortbyratings = {'result': []}
 	try:
-		datum = redis_con.get('tvf_json')
+		datum = REDIS_CONN.get('tvf_json')
 		datum = ast.literal_eval(datum)
 		if 'sort' not in request.GET:
-			response = {'status':'Request Failure','message':'Incorrect request. The Sort param is missing/invalid in the request'}
-			return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=403)
+			response = {'status': 'Request Failure', \
+					'message': 'Incorrect request. The Sort \
+					param is missing/invalid in the request'}
+			return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), \
+					content_type='application/json', status=400)
 		else:
 			if request.GET['sort'] == '':
-				response = {'status':'Request Failure','message':'Incorrect request. The Sort param is missing/invalid in the request'}
-				return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=403)
+				response = {'status': 'Request Failure', \
+						'message': 'Incorrect request. The Sort param is \
+						missing/invalid in the request'}
+				return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder),\
+					 content_type='application/json', status=400)
 
 		if request.GET['sort'] == 'asc':
 			lines = sorted(datum, key=lambda k: k['rating'], reverse=False)
@@ -98,11 +128,9 @@ def ratingsort(request):
 		else:
 			lines = sorted(datum, key=lambda k: k['rating'], reverse=True)
 			sortbyratings['result'].append(lines)
-		return HttpResponse(json.dumps(sortbyratings, cls=DjangoJSONEncoder), content_type='application/json', status=200)
-	except Exception, e:
-		response = {'status':'Request Failure','message':e}
-		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), content_type='application/json', status=500)
-
-				
-
-
+		return HttpResponse(json.dumps(sortbyratings, cls=DjangoJSONEncoder), \
+				content_type='application/json', status=200)
+	except Exception, exc:
+		response = {'status': 'Request Failure', 'message': exc}
+		return HttpResponse(json.dumps(response, cls=DjangoJSONEncoder), \
+				content_type='application/json', status=500)
