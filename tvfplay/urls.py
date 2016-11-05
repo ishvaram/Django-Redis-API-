919e2427b1cbc7ef3f163acd00b5445045ca176b
ishvaram <jehovaram@gmail.com>
2016-11-05 21:13:30 +0530

    Added a Procfile.

--- tvfplay/urls.py
+++ tvfplay/urls.py
@@ -1,6 +1,6 @@
 import sys,os
 file_path = os.path.dirname(__file__).rsplit('/', 1)[0]
-sys.path.append(file_path + '/tvfplay/views/')
+sys.path.append(file_path + '/tvfplay/views')
 from django.conf.urls import url
 from . import views
 from tvfredis import index, category, all_items, listbytype, ratingsort
