a0359929ee8521e329bb0da0a6aded6133e34053
ishvaram <jehovaram@gmail.com>
2016-11-06 00:14:33 +0530

    final commit to repo

--- tvfplay/views/tvfredis.py
+++ tvfplay/views/tvfredis.py
@@ -24,16 +24,16 @@ except Exception, exc:
 	REDIS_CONN = None
 
 
-def redoo(request):
-	REDIS_CONN = redis.from_url(os.environ.get("REDIS_URL"))
-	REDIS_CONN_DATA = REDIS_CONN.keys()
-	return REDIS_CONN_DATA 
+# def redoo(request):
+# 	REDIS_CONN = redis.from_url(os.environ.get("REDIS_URL"))
+# 	REDIS_CONN_DATA = REDIS_CONN.keys()
+# 	return REDIS_CONN_DATA 
 
-def insert(request):
-	with open('interview.json') as data_file:
-		test_data = json.load(data_file)
-		REDIS_CONN.set('tvf_json', test_data)
-	return True
+# def insert(request):
+# 	with open('interview.json') as data_file:
+# 		test_data = json.load(data_file)
+# 		REDIS_CONN.set('tvf_json', test_data)
+# 	return True
 
 
 def index(request):
